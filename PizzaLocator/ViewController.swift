9c246354e3070403a44f85327504dd4d1c5623f8
m3libea <rociorommor@gmail.com>
2016-02-11 14:12:40 -0800

    added GeoLocation

--- PizzaLocator/ViewController.swift
+++ PizzaLocator/ViewController.swift
@@ -7,8 +7,13 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    @IBOutlet var mapView:MKMapView?
+    var locationManager:CLLocationManager?
+    let distanceSpan:Double = 500
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +23,33 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if let mapView = self.mapView
+        {
+            mapView.delegate = self
+        }
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        if locationManager == nil{
+            locationManager = CLLocationManager()
+            locationManager!.delegate = self
+            locationManager!.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+            locationManager!.requestAlwaysAuthorization()
+            locationManager!.distanceFilter = 50
+            locationManager!.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation) {
+        if let mapView = self.mapView {
+            let region = MKCoordinateRegionMakeWithDistance(newLocation.coordinate, distanceSpan, distanceSpan)
+            mapView.setRegion(region, animated: true)
+        }
     }
 
 
--- Podfile
+++ Podfile
@@ -2,5 +2,5 @@ source 'https://github.com/CocoaPods/Specs.git'
 platform :ios, '8.0'
 use_frameworks!
  
-pod 'QuadratTouch', '1.1.1'
+pod 'QuadratTouch', '>= 1.0'
 pod 'RealmSwift'
